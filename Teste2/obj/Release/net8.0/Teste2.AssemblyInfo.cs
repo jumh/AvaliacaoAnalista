4e1620778c5893366f1465cb0d518cedfd5df953
joaocaju1 <jgpretti2002@gmail.com>
2025-05-07 17:21:53 -0300

    Teste BTP - João Guilherme Pretti Genari

--- Teste2/obj/Release/net8.0/Teste2.AssemblyInfo.cs
+++ Teste2/obj/Release/net8.0/Teste2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Teste1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Teste2")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56687dc7bd19eeab9824be095f123fc3a27c94a5")]
-[assembly: System.Reflection.AssemblyProductAttribute("Teste1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Teste1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Teste2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Teste2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
