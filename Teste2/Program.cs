15f28932bf2e0162d714e4d2f498039c7857045d
rodrigo.cambraia <rodrigo.cambraia@ascsolutions.com.br>
2025-10-01 22:58:24 -0300

    Rodrigo Cambraia - teste técnico

--- Teste2/Program.cs
+++ Teste2/Program.cs
@@ -12,7 +12,61 @@ namespace Teste2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Boa sorte!");
+            string? userInput;
+
+            //loop até que o input do usuário seja válido
+            int number;
+            while (true)
+            {
+                Console.Write("Digite um número inteiro, positivo, maior que 0 e menor que 20: ");
+
+                var input = Console.ReadLine();
+
+                var result = GetValidInput(input);
+
+                if (result.HasValue)
+                {
+                    number = result.Value;
+                    break;
+                }
+
+                Console.WriteLine("Entrada inválida. Tente novamente.\n");
+            }
+
+            // crio o array com o tamanho informado pelo usuário. Não há necessidade de me preocupar com overflow e uso de long por ex.
+            // uma vez que o input é limitado à 20
+            int[] fibonacciArray = new int[number];
+
+            for (int i = 0; i < number; i++)
+            {
+                //os dois primeiros números da sequência são sempre 1
+                if (i is 0 || i is 1)
+                {
+                    fibonacciArray[i] = 1;
+
+                    //aqui garanto o early continue, para evitar acessar índices negativos
+                    continue;
+                }
+
+                //aqui eu garanto que o índice 0 e 1 já foram tratados, então posso acessar os dois índices anteriores sem validação
+                //optei por esse formato de cálculo, pois entendo que melhora a legibilidade do código, além de economizar memória declarando menos variáveis
+                fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+            }
+
+            Console.WriteLine($"Sequência de Fibonacci: {string.Join(",", fibonacciArray)}");
+        }
+
+        static int? GetValidInput(string? userInput)
+        {
+            //verifico se é nulo ou vazio
+            if (!int.TryParse(userInput, out int number))
+                return null;
+
+            //verifico se cumpre o requisito de ser maior que 0 e menor que 20
+            if (number <= 0 || number >= 20)
+                return null;
+
+            return number;
         }
     }
 }
