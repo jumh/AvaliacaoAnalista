04def6e831d5995bf148fc1cbc8579c659bb4e6c
GustaGitHub <gustavo.ribeiro.duarte2003@gmail.com>
2026-08-24 23:45:22 -0300

    feat: resolução do teste 1

--- Teste1/Program.cs
+++ Teste1/Program.cs
@@ -5,13 +5,49 @@
     Exemplo: AAABCCDDD -> Retorno : ABCD
  */
 
+using System;
+
 namespace Teste1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Boa sorte!");
+            Console.WriteLine("Digite uma palavra (letras/numeros)");
+            string? palavraDigitada = Console.ReadLine();
+
+            if (!ValidarPalavraDigitada(palavraDigitada)) return;
+
+            string palavraSemDuplicidade = RemoverCaracteresDuplicadaos(palavraDigitada);
+
+            Console.WriteLine($"Palavra sem repetições: {palavraSemDuplicidade}");
+        }
+
+        static string RemoverCaracteresDuplicadaos(string palavra)
+        {
+            string palavraSemDuplicidade = palavra[0].ToString();   //Inicializando variável para iteração 
+
+            for (int c = 1; c < palavra.Count(); c++)
+            {
+                var letraAtual = palavra[c];
+                var letraAnterior = palavraSemDuplicidade[palavraSemDuplicidade.Count() - 1];
+
+                if(letraAnterior != letraAtual)
+                    palavraSemDuplicidade += letraAtual;    
+            }
+
+            return palavraSemDuplicidade;
+        }
+
+        static bool ValidarPalavraDigitada(string? palavra)
+        {
+            if (String.IsNullOrEmpty(palavra.Trim()))
+            {
+                Console.WriteLine("Palavra não iniciada");
+                return false;
+            }
+
+            return true;
         }
     }
 }
